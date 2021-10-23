8069ec6c2b4d023c2261f0f35e637fbb0b1f21b0
Gerónimo <geroortizz123@gmail.com>
2021-10-22 23:15:17 -0300

    Se añade lógica de actualización y eliminación de datos

--- src/service.js
+++ src/service.js
@@ -1,19 +1,52 @@
-const data = require('./MOCK_DATA.json');
+const data = require("./MOCK_DATA.json");
 
 /**
  *  Creamos esta capa de servicio para obtener los datos, creamos el objeto que puede ser exportado
  *  para luego obtenerlo desde el index
  */
 
-module.exports = {   //esto nos dice que service.js es un modulo que puede ser exportado
+module.exports = {
+  //esto nos dice que service.js es un modulo que puede ser exportado
 
-    getUsers: () => data,
-    createUser: (user) => {
-        let newUser = {
-            id: data.length + 1,
-            ...user
+  getUsers: () => data,
+  getUserById: (id) => {
+    let idNumerico = Number(id);
+    let user = data.filter((persona) => persona.id === idNumerico)[0];
+    /*  
+            recorremos todo el arreglo 'data' con la variable
+            persona, y retornamos aquella que coincide
+        */
+    return user;
+  },
+  createUser: (user) => {
+    let newUser = {
+      id: data.length + 1,
+      ...user,
+    };
+    data.push(newUser);
+    return newUser;
+  },
+  updateUser: (id, user) => {
+    let idNumerico = Number(id);
+    let newUser = {};
+    data.forEach((element) => {
+      if (element.id == idNumerico) {
+
+        newUser = {
+          id: idNumerico,
+          first_name: user.first_name,
+          last_name: user.last_name,
+          email: user.email
         };
-        data.push(newUser);
-        return newUser;
-    },
+        data[idNumerico - 1] = newUser; 
+      }
+    });
+    return newUser;
+  },
+  deleteUser: (id) => {
+    let idNumerico = Number(id);
+    data.splice(idNumerico-1,1);
+  },
+
+
 };
